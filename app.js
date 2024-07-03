9ae45463ff50b67342b83f2eb2b72927c05ed5e1
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-04 01:35:14 +0700

    Update app.js Revise00 first issued

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// write code here
+let user = prompt("Enter username:");
+let pass = '';
+
+if(user == "codecamp" ){
+    pass = prompt("Enter password")
+    if(pass !== '123456'){alert("Wrong password"); user = 'guest'; alert(`Hello ${user}`);}
+       else if(pass = '123456'){alert(`Hello ${user}`)}
+}
+else{user = 'guest'; alert(`Hello ${user}`);}
+
+
+console.log(`username : ${user}`)
+console.log(`password : ${pass}`);
